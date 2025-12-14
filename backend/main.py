4abfb732f39923618530275a48b0e840119d874e
dev-celeste <devcelestecodes@gmail.com>
2025-12-14 15:30:50 -0500

    Load environment variables for Spotify config

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
 from routers import health
 
-app = FastAPI(title="My Music Universe API")
+load_dotenv()
 
+app = FastAPI(title="My Music Universe API")
 
 app.include_router(health.router)
