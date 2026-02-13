71bbc64a8fc086ec637463039bb8ac513a32e1fd
dev-celeste <devcelestecodes@gmail.com>
2026-02-12 23:58:33 -0500

    health check

--- backend/main.py
+++ backend/main.py
@@ -8,10 +8,18 @@ from routers import health, auth
 load_dotenv()
 
 app = FastAPI(title="Tuniverse API")
+
 @app.get("/")
 def root():
     return {"message": "ts running"}
 
+@app.get("/api/v1")
+def api_root():
+    return {"message": "API v1 is liveee"}
+
+@app.get("/api/v1/health")
+def health_check():
+    return {"status": "ok"}
 
 app.include_router(health.router, prefix="/api/v1")
 app.include_router(auth.router, prefix="/api/v1")
